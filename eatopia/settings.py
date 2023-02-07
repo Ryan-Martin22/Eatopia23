9f1ae7204effef4c8f6b4e43e17caadae471c551
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-02-07 15:24:17 +0000

    add change to cloudinary url

--- eatopia/settings.py
+++ eatopia/settings.py
@@ -138,6 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+#  https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
