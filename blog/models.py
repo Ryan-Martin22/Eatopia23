7da4629daa9eb0f58b91fe68fe0f0de216ee1047
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-02-10 17:24:44 +0000

    add fixed error to datbase model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
 
+
 class Recipe_model(models.Model):
     """
     Model for recipe
@@ -23,8 +24,8 @@ class Recipe_model(models.Model):
                                                    MinValueValidator(1),
                                                    MaxValueValidator(120)
                                                    ])
-    ingredients_list = SummernoteTextField()
-    instructions = SummernoteTextField()
+    ingredients_list = models.TextField()
+    instructions = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='recipe_likes',
--- eatopia/settings.py
+++ eatopia/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote',
     'blog',
 ]
 
