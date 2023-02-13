180b4fd4a416d42e8d9e0048d9f478c0cdd19add
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2023-02-13 14:49:22 +0000

    add templates,css folders and created views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import RecipeModel
 
-# Create your views here.
+
+class RecipeModelList(generic.ListView):
+    model = RecipeModel
+    queryset = RecipeModel.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
+
